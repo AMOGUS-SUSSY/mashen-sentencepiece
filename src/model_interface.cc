087bf495bfae22b03660895ff422538642071b3f
mashen <mashen@student.ethz.ch>
2024-06-13 16:43:28 +0200

    Now adds whitespace at the start

--- src/model_interface.cc
+++ src/model_interface.cc
@@ -18,6 +18,7 @@
 
 #include "sentencepiece_model.pb.h"
 #include "third_party/absl/strings/str_format.h"
+#include "third_party/absl/strings/str_cat.h"
 #include "util.h"
 
 namespace sentencepiece {
@@ -209,30 +210,47 @@ std::vector<absl::string_view> SplitIntoWords(absl::string_view text,
 
     // BOTH
     // ATTENTION: First word of sentence is no prefixed. Must find a way to insert ws at start.
+    char temp[200];
+    memcpy(temp, absl::StrCat(kSpaceSymbol,begin).data(), 199);
     if (begin < end) {
       result.emplace_back(begin, 0);
     }
     while (begin < end) {
-      const int mblen =
-          std::min<int>(string_util::OneCharLen(begin), end - begin);
+      const int mblen = 
+        std::min<int>(string_util::OneCharLen(begin), end - begin);
       const bool is_ws = absl::string_view(begin, mblen) == kSpaceSymbol;
+      
 
+      // insert suffixed whitespace
       if (is_ws) {
+
         result.back() =
           absl::string_view(result.back().data(), result.back().size() + mblen);
         result.emplace_back(begin, 0);
+
       }
       
+      // insert duplicated prefixed whitespace
       // only duplicate whitespace if not at end of sentence.
       if (begin + mblen < end) {
-        result.back() =
-          absl::string_view(result.back().data(), result.back().size() + mblen);
+
+        if (begin == text.data()) {
+
+          result.emplace_back(temp);
+          result.back() =
+            absl::string_view(result.back().data(), kSpaceSymbol.length() + mblen);
+
+        } else {
+          result.back() =
+            absl::string_view(result.back().data(), result.back().size() + mblen);
+        }
+        
       }
       begin += mblen;
     }
 
-    //LOGGING
-    LOG(INFO) << result;
+    // //LOGGING
+    // LOG(INFO) << result;
 
   } else { 
     
